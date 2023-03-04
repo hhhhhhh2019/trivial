689a0d993fd7472d8a0e9fae96e4fe5f4f9b91a2
Алексей <a.galov@vk.com>
2023-03-04 10:55:10 +0300

    Gaul nums and polynomials

---
+++ GaulNum.py
@@ -0,0 +1,54 @@
+class GaulNum:
+	log = [0,0,1,4,2,8,5,10, 3,14, 9,7, 6,13,11,12]
+	pow = [  1,2,4,8,3,6,12,11, 5,10,7,14,15,13, 9]
+
+	def __init__(self, v):
+		if isinstance(v, int):
+			self.value = v
+		else:
+			raise TypeError("Ожидается число, а получено " + str(type(v)))
+
+
+	def __add__(self, other):
+		if not isinstance(other, GaulNum):
+			raise TypeError("Невозможно сложить с " + str(type(other)))
+
+		return GaulNum(self.value ^ other.value)
+
+	def __sub__(self, other):
+		if not isinstance(other, GaulNum):
+			raise TypeError("Невозможно сложить с " + str(type(other)))
+
+		return GaulNum(self.value ^ other.value)
+
+	def __mul__(self, other):
+		if not isinstance(other, GaulNum):
+			raise TypeError("Невозможно умножить на " + str(type(other)))
+
+		if self.value == 0 or other.value == 0:
+			return GaulNum(0)
+
+		return GaulNum(GaulNum.pow[(GaulNum.log[self.value] + GaulNum.log[other.value]) % 15])
+
+	def __truediv__(self, other):
+		if not isinstance(other, GaulNum):
+			raise TypeError("Невозможно разделить на " + str(type(other)))
+
+		if other.value == 0:
+			raise ZeroDivisionError
+
+		if self.value == 0:
+			return GaulNum(0)
+
+		return GaulNum(GaulNum.pow[(GaulNum.log[self.value] + 15 - GaulNum.log[other.value]) % 15])
+
+
+	def __str__(self):
+		return str(self.value)
+
+
+if __name__ == "__main__":
+	a = GaulNum(8)
+	b = GaulNum(4)
+
+	print(a * b)
---
+++ GaulPoly.py
@@ -0,0 +1,135 @@
+from GaulNum import GaulNum
+from operator import add, sub
+
+
+class GaulPoly:
+	def __init__(self, data):
+		if isinstance(data[0], int):
+			self.data = [GaulNum(i) for i in data]
+		elif isinstance(data[0], GaulNum):
+			self.data = data
+		else:
+			raise TypeError
+
+	def __str__(self):
+		return ' '.join([str(i) for i in self.data])
+
+	def __getitem__(self, key):
+		return self.data[key]
+
+	def __setitem__(self, key, value):
+		if type(self.data[0]) != type(value):
+			raise TypeError
+
+		self.data[key] = value
+
+
+	def __len__(self):
+		return len(self.data)
+
+
+	def __add__(self, other):
+		if not isinstance(other, GaulPoly):
+			raise TypeError("Невозможно сложить с " + str(type(other)))
+
+		a = [GaulNum(0)] * max(0, len(other)-len(self)) + self.data
+		b = [GaulNum(0)] * max(0, len(self)-len(other)) + other.data
+
+		return GaulPoly(list(map(add, a, b)))
+
+	def __sub__(self, other):
+		if not isinstance(other, GaulPoly):
+			raise TypeError("Невозможно вычесть " + str(type(other)))
+
+		a = [GaulNum(0)] * max(0, len(other)-len(self)) + self.data
+		b = [GaulNum(0)] * max(0, len(self)-len(other)) + other.data
+
+		return GaulPoly(list(map(sub, a, b)))
+
+	def __mul__(self, other):
+		if not isinstance(other, GaulPoly):
+			raise TypeError("Невозможно умножить на " + str(type(other)))
+
+		result = GaulPoly([0] * max(len(other), len(self)))
+
+		for i,v in enumerate(other[::-1]):
+			s = GaulPoly([*([j*v for j in self]), *([GaulNum(0)]*i)])
+			result += s
+
+		return result
+
+	def __floordiv__(self, other):
+		if not isinstance(other, GaulPoly):
+			raise TypeError("Невозможно разделить на " + str(type(other)))
+
+		result = []
+
+		a = GaulPoly(self[::])
+		b = other[::]
+
+
+		for i in range(len(b)):
+			if b[0].value != 0:
+				break
+
+			b.pop(0)
+
+		b = GaulPoly(b)
+
+
+		for i in range(len(a)-1):
+			m = a[i] / b[0]
+
+			result.append(m)
+
+			s = GaulPoly([*([j*m for j in b]), *([GaulNum(0)] * (len(b)-i))])
+
+			a -= s
+
+		return GaulPoly(result)
+
+	def __mod__(self, other):
+		if not isinstance(other, GaulPoly):
+			raise TypeError("Невозможно разделить на " + str(type(other)))
+
+		result = []
+
+		a = GaulPoly(self[::])
+		b = other[::]
+
+
+		for i in range(len(b)):
+			if b[0].value != 0:
+				break
+
+			b.pop(0)
+
+		b = GaulPoly(b)
+
+
+		for i in range(len(a)-1):
+			m = a[i] / b[0]
+
+			result.append(m)
+
+			s = GaulPoly([*([j*m for j in b]), *([GaulNum(0)] * (len(b)-i))])
+
+			a -= s
+
+
+		result = a[::]
+
+		for i in range(len(result)):
+			if result[0].value != 0:
+				break
+
+			result.pop(0)
+
+		return GaulPoly(result)
+
+
+if __name__ == "__main__":
+	a = GaulPoly([1,1,0,1])
+	b = GaulPoly([0,0,1,1])
+
+	print(a//b)
